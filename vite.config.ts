d38e381998026529bdd3abd7c2c0c78c483a984b
Freud <>
2024-12-16 16:12:32 -0500

    correction de bug avec base

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vuJS/',
+  base: '/vuJS/',  
   plugins: [
     vue(),
     VitePWA({
-      // add this to cache all the imports
       workbox: {
         globPatterns: ["**/*"],
       },
-      // add this to cache all the
-      // static assets in the public folder
       includeAssets: [
         "**/*",
       ],
@@ -23,29 +19,29 @@ export default defineConfig({
         "theme_color": "#ff0000",
         "background_color": "#ff0000",
         "display": "standalone",
-        "scope": "/",
-        "start_url": "/",
+        "scope": "/vuJS/",  
+        "start_url": "/vuJS/",  
         "name": "Application d'affichage de Produits",
         "short_name": "Liste de Produits",
-        "description": "Labo1 affichage liste de produits utilisant bootstrap et vuJs",
+        "description": "Labo1 affichage liste de produits utilisant bootstrap et vueJs",
         "icons": [
           {
-            "src": "/icon-192x192.png",
+            "src": "icons/icon-192x192.png",  
             "sizes": "192x192",
             "type": "image/png"
           },
           {
-            "src": "/icon-256x256.png",
+            "src": "icons/icon-256x256.png",  
             "sizes": "256x256",
             "type": "image/png"
           },
           {
-            "src": "/icon-384x384.png",
+            "src": "icons/icon-384x384.png", 
             "sizes": "384x384",
             "type": "image/png"
           },
           {
-            "src": "/icon-512x512.png",
+            "src": "icons/icon-512x512.png",  
             "sizes": "512x512",
             "type": "image/png"
           }
